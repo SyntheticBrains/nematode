5ea9b8b03662e079942946dedb307cd9c57e94b0
Chris Zaharia <chris.zaharia@gmail.com>
2025-04-26 21:50:52 +1000

    Add dynamic brain to brain submodule import

--- packages/quantum-nematode/quantumnematode/brain/__init__.py
+++ packages/quantum-nematode/quantumnematode/brain/__init__.py
@@ -1,8 +1,9 @@
 """Module for quantum brain architectures."""
 
 from .complex import ComplexBrain
+from .dynamic import DynamicBrain
 from .memory import MemoryBrain
 from .reduced import ReducedBrain
 from .simple import SimpleBrain
 
-__all__ = ["ComplexBrain", "MemoryBrain", "ReducedBrain", "SimpleBrain"]
+__all__ = ["ComplexBrain", "DynamicBrain", "MemoryBrain", "ReducedBrain", "SimpleBrain"]
