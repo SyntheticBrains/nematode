497085baa4f97c066b44c62e1d3d12e443527573
Chris Zaharia <chris.zaharia@gmail.com>
2025-04-27 13:31:47 +1000

    Add plot for last cum rewards

--- packages/quantum-nematode/quantumnematode/report.py
+++ packages/quantum-nematode/quantumnematode/report.py
@@ -8,7 +8,7 @@ from quantumnematode.logging_config import (  # pyright: ignore[reportMissingImp
 def summary(
     num_runs: int,
     max_steps: int,
-    all_results: list[tuple[int, int, list[tuple], int]],
+    all_results: list[tuple[int, int, list[tuple], float, float]],
 ) -> None:
     """
     Print a summary of the simulation results.
@@ -20,12 +20,12 @@ def summary(
     all_results : list[tuple[int, int, list[tuple]]]
         A list of tuples containing the run number, number of steps taken, and path taken.
     """
-    average_steps = sum(steps for _, steps, _, _ in all_results) / num_runs
+    average_steps = sum(steps for _, steps, _, _, _ in all_results) / num_runs
     improvement_rate = (all_results[0][1] - all_results[-1][1]) / all_results[0][1] * 100
-    success_rate = sum(1 for _, steps, _, _ in all_results if steps < max_steps) / num_runs * 100
+    success_rate = sum(1 for _, steps, _, _, _ in all_results if steps < max_steps) / num_runs * 100
 
     print("All runs completed:")  # noqa: T201
-    for run, steps, _path, _total_reward in all_results:
+    for run, steps, _path, _total_reward, _last_total_reward in all_results:
         print(f"Run {run}: {steps} steps")  # noqa: T201
 
     print(f"Average steps per run: {average_steps:.2f}")  # noqa: T201
@@ -34,7 +34,7 @@ def summary(
 
     if not logger.disabled:
         logger.info("All runs completed:")
-        for run, steps, path, _total_reward in all_results:
+        for run, steps, path, _total_reward, _last_total_reward in all_results:
             logger.info(f"Run {run}: {steps} steps, Path: {path}")
 
         logger.info(f"Average steps per run: {average_steps:.2f}")
