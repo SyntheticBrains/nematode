7fd41487dad9579d3e27918e4bd54f0abb6e9759
Chris Zaharia <chris.zaharia@gmail.com>
2025-07-06 18:01:34 +1000

    Add enum for corners

--- packages/quantum-nematode/quantumnematode/brain/arch/dtypes.py
+++ packages/quantum-nematode/quantumnematode/brain/arch/dtypes.py
@@ -1,10 +1,15 @@
-from typing import Literal
+"""Define the types of brains used in the quantum nematode project."""
+
 from enum import Enum
+from typing import Literal
 
 
 class BrainType(Enum):
+    """Enum for different types of brains."""
+
     MODULAR = "modular"
     MLP = "mlp"
+
 
 BRAIN_TYPES = Literal[BrainType.MODULAR, BrainType.MLP]
 QUANTUM_BRAIN_TYPES = Literal[BrainType.MODULAR]
