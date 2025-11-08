e6f39b763df39fa0e52a69fd71e3a603d5b052b8
Chris Zaharia <chris.zaharia@gmail.com>
2025-11-09 08:42:48 +1100

    Add experiment metadata

---
+++ packages/quantum-nematode/quantumnematode/experiment/__init__.py
@@ -0,0 +1,19 @@
+"""Experiment tracking and metadata management for Quantum Nematode simulations."""
+
+from quantumnematode.experiment.metadata import (
+    BenchmarkMetadata,
+    BrainMetadata,
+    EnvironmentMetadata,
+    ExperimentMetadata,
+    ResultsMetadata,
+    SystemMetadata,
+)
+
+__all__ = [
+    "BenchmarkMetadata",
+    "BrainMetadata",
+    "EnvironmentMetadata",
+    "ExperimentMetadata",
+    "ResultsMetadata",
+    "SystemMetadata",
+]
