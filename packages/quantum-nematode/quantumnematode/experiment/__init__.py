d789d5c95b7ff46da1feb9706a5c9444b82e4142
Chris Zaharia <chris.zaharia@gmail.com>
2025-12-28 22:42:33 +1100

    Update experiment submodule init

--- packages/quantum-nematode/quantumnematode/experiment/__init__.py
+++ packages/quantum-nematode/quantumnematode/experiment/__init__.py
@@ -6,6 +6,7 @@ from quantumnematode.experiment.metadata import (
     EnvironmentMetadata,
     ExperimentMetadata,
     ResultsMetadata,
+    StatValue,
     SystemMetadata,
 )
 from quantumnematode.experiment.storage import (
@@ -14,18 +15,35 @@ from quantumnematode.experiment.storage import (
     load_experiment,
     save_experiment,
 )
+from quantumnematode.experiment.submission import (
+    MIN_RUNS_PER_SESSION,
+    MIN_SESSIONS_REQUIRED,
+    AggregateMetrics,
+    NematodeBenchSubmission,
+    SessionReference,
+)
 from quantumnematode.experiment.tracker import capture_experiment_metadata
+from quantumnematode.experiment.validation import (
+    validate_submission,
+)
 
 __all__ = [
+    "MIN_RUNS_PER_SESSION",
+    "MIN_SESSIONS_REQUIRED",
+    "AggregateMetrics",
     "BenchmarkMetadata",
     "BrainMetadata",
     "EnvironmentMetadata",
     "ExperimentMetadata",
+    "NematodeBenchSubmission",
     "ResultsMetadata",
+    "SessionReference",
+    "StatValue",
     "SystemMetadata",
     "capture_experiment_metadata",
     "compare_experiments",
     "list_experiments",
     "load_experiment",
     "save_experiment",
+    "validate_submission",
 ]
