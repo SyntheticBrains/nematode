43ccbd3430ae79f5c2397b0c5a284a6989655644
Chris Zaharia <chris.zaharia@gmail.com>
2026-01-01 18:31:50 +1100

    Refactor modules
    - Rename modules to better match their scientific function
    - Add additional details in docstrings

--- packages/quantum-nematode/quantumnematode/brain/__init__.py
+++ packages/quantum-nematode/quantumnematode/brain/__init__.py
@@ -3,9 +3,11 @@
 from .arch.mlp import MLPBrain
 from .arch.modular import ModularBrain
 from .arch.spiking import SpikingBrain
+from .modules import ModuleName
 
 __all__ = [
     "MLPBrain",
     "ModularBrain",
+    "ModuleName",
     "SpikingBrain",
 ]
